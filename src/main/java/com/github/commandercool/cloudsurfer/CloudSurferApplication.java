55656b63f71a85f64aca5fcd0654881cec82f1e3
alerokhi <Aleksandr.Erokhin@t-systems.com>
2020-05-31 14:32:28 +0300

    Populate with template from start.spring.io/

---
+++ src/main/java/com/github/commandercool/cloudsurfer/CloudSurferApplication.java
@@ -0,0 +1,13 @@
+package com.github.commandercool.cloudsurfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSurferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSurferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/commandercool/cloudsurfer/CloudSurferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.commandercool.cloudsurfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSurferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
