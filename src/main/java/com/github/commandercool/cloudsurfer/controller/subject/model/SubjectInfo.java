24d6c368b668df2688a4d26911cc84fee1f2b60a
alerokhi <Aleksandr.Erokhin@t-systems.com>
2020-06-03 22:11:53 +0300

    Include all steps in subject info

--- src/main/java/com/github/commandercool/cloudsurfer/controller/subject/model/SubjectInfo.java
+++ src/main/java/com/github/commandercool/cloudsurfer/controller/subject/model/SubjectInfo.java
@@ -18,6 +18,24 @@ import lombok.Setter;
 @Setter
 public class SubjectInfo {
 
+    private static final List<String> STEPS =
+            Arrays.asList("MotionCor", "Talairach ", "Talairach Failure Detection", "Nu Intensity Correction",
+                    "Intensity Normalization", "Skull Stripping", "EM Registration", "CA Normalize", "CA Reg",
+                    "SubCort Seg", "Merge ASeg", "Intensity Normalization2", "Mask BFS", "WM Segmentation", "Fill",
+                    "Tessellate lh", "Tessellate rh", "Smooth1 lh", "Smooth1 rh", "Inflation1 lh", "Inflation1 rh",
+                    "QSphere lh", "QSphere rh", "Fix Topology Copy lh", "Fix Topology Copy rh", "Fix Topology lh",
+                    "Fix Topology rh", "Make White Surf lh", "Make White Surf rh", "Smooth2 lh", "Smooth2 rh",
+                    "Inflation2 lh", "Inflation2 rh", "Curv .H and .K lh", "Curv .H and .K rh", "Curvature Stats lh",
+                    "Curvature Stats rh", "Sphere lh", "Sphere rh", "Surf Reg lh", "Surf Reg rh", "Jacobian white lh",
+                    "Jacobian white rh", "AvgCurv lh", "AvgCurv rh", "Cortical Parc lh", "Cortical Parc rh",
+                    "Make Pial Surf lh", "Make Pial Surf rh", "Surf Volume lh", "Surf Volume rh",
+                    "Cortical ribbon mask", "Parcellation Stats lh", "Parcellation Stats rh", "Cortical Parc 2 lh",
+                    "Cortical Parc 2 rh", "Parcellation Stats 2 lh", "Parcellation Stats 2 rh", "Cortical Parc 3 lh",
+                    "Cortical Parc 3 rh", "Parcellation Stats 3 lh", "Parcellation Stats 3 rh", "WM/GM Contrast lh",
+                    "WM/GM Contrast rh", "Relabel Hypointensities", "AParc-to-ASeg aparc", "AParc-to-ASeg a2009s",
+                    "AParc-to-ASeg DKTatlas", "APas-to-ASeg", "ASeg Stats", "WMParc", "BA_exvivo Labels lh",
+                    "BA_exvivo Labels rh");
+
     @JsonProperty("name")
     private String name;
 
@@ -59,6 +77,11 @@ public class SubjectInfo {
                     steps.get(steps.size() - 1).setStatus(2);
                 }
             }
+            if (steps.size() < STEPS.size()) {
+                STEPS.subList(steps.size(), STEPS.size()).forEach(s -> {
+                    steps.add(new ReconAllStep(s, null, 0));
+                });
+            }
             subjectInfo.setStepStatList(steps);
         } else {
             subjectInfo.setStatus(0);
@@ -83,7 +106,7 @@ public class SubjectInfo {
     private static LocalDateTime getDateTime(String full) {
         int length = full.length();
         String dateTimeStr = full.substring(length - 28, length);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEE LLL dd HH:mm:ss v uuuu")
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEE LLL  d HH:mm:ss v uuuu")
                 .withLocale(Locale.US);
         return LocalDateTime.parse(dateTimeStr, dateTimeFormatter);
     }
