620dbb511401aaabfd783b084dacb99966783fea
alerokhi <Aleksandr.Erokhin@t-systems.com>
2020-08-30 16:44:23 +0300

    Fix param for aseg command

--- src/main/java/com/github/commandercool/cloudsurfer/docker/DockerService.java
+++ src/main/java/com/github/commandercool/cloudsurfer/docker/DockerService.java
@@ -70,7 +70,7 @@ public class DockerService {
     private String getSubjects(List<String> subjects) {
         StringBuilder paramBuilder = new StringBuilder();
         subjects.forEach(s -> {
-            paramBuilder.append(paramBuilder);
+            paramBuilder.append(s);
             paramBuilder.append(" ");
         });
         return paramBuilder.toString();
